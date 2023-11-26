eba68a6c4242b262ca1b09aa6ba276f2df1fcaa1
Chriss <71105027+da-Jimbo@users.noreply.github.com>
2023-11-26 02:40:12 +0100

    Update AdapterUpdates.js

--- Sicherheit/AdapterUpdates.js
+++ Sicherheit/AdapterUpdates.js
@@ -1,10 +1,46 @@
-on({id: "admin.0.info.newUpdates"/*Indicator if new adapter updates are available*/, val: true}, async function (obj) {
-  let value = obj.state.val;
-  let oldValue = obj.oldState.val;
-  sendTo("telegram.0", "send", {
-      text: (['Es gibt ioBroker Adapter Updates: ','\n',getState("admin.0.info.updatesList").val].join('')),
-      user: 'Chriss'
-  });
-});
-
-//JTNDeG1sJTIweG1sbnMlM0QlMjJodHRwcyUzQSUyRiUyRmRldmVsb3BlcnMuZ29vZ2xlLmNvbSUyRmJsb2NrbHklMkZ4bWwlMjIlM0UlM0NibG9jayUyMHR5cGUlM0QlMjJvbiUyMiUyMGlkJTNEJTIyTjloRnNCKGMwWSU1RGtlTCU3QjElMkIwJTNEbSUyMiUyMHglM0QlMjI2MyUyMiUyMHklM0QlMjI4OCUyMiUzRSUzQ2ZpZWxkJTIwbmFtZSUzRCUyMk9JRCUyMiUzRWFkbWluLjAuaW5mby5uZXdVcGRhdGVzJTNDJTJGZmllbGQlM0UlM0NmaWVsZCUyMG5hbWUlM0QlMjJDT05ESVRJT04lMjIlM0V0cnVlJTNDJTJGZmllbGQlM0UlM0NmaWVsZCUyMG5hbWUlM0QlMjJBQ0tfQ09ORElUSU9OJTIyJTNFJTNDJTJGZmllbGQlM0UlM0NzdGF0ZW1lbnQlMjBuYW1lJTNEJTIyU1RBVEVNRU5UJTIyJTNFJTNDYmxvY2slMjB0eXBlJTNEJTIydGVsZWdyYW0lMjIlMjBpZCUzRCUyMllidk83JTdDNExOUyU3Q3FEai1nNjAlMjVKJTIyJTNFJTNDZmllbGQlMjBuYW1lJTNEJTIySU5TVEFOQ0UlMjIlM0UuMCUzQyUyRmZpZWxkJTNFJTNDZmllbGQlMjBuYW1lJTNEJTIyTE9HJTIyJTNFJTNDJTJGZmllbGQlM0UlM0NmaWVsZCUyMG5hbWUlM0QlMjJTSUxFTlQlMjIlM0VGQUxTRSUzQyUyRmZpZWxkJTNFJTNDZmllbGQlMjBuYW1lJTNEJTIyUEFSU0VNT0RFJTIyJTNFZGVmYXVsdCUzQyUyRmZpZWxkJTNFJTNDZmllbGQlMjBuYW1lJTNEJTIyRVNDQVBJTkclMjIlM0VGQUxTRSUzQyUyRmZpZWxkJTNFJTNDZmllbGQlMjBuYW1lJTNEJTIyRElTQUJMRV9XRUJfUEFHRV9QUkVWSUVXJTIyJTNFRkFMU0UlM0MlMkZmaWVsZCUzRSUzQ3ZhbHVlJTIwbmFtZSUzRCUyMk1FU1NBR0UlMjIlM0UlM0NzaGFkb3clMjB0eXBlJTNEJTIydGV4dCUyMiUyMGlkJTNEJTIyMXBYS043ZCUzRnkueGFHKXklM0RlcVh5JTIyJTNFJTNDZmllbGQlMjBuYW1lJTNEJTIyVEVYVCUyMiUzRXRleHQlM0MlMkZmaWVsZCUzRSUzQyUyRnNoYWRvdyUzRSUzQ2Jsb2NrJTIwdHlwZSUzRCUyMnRleHRfam9pbiUyMiUyMGlkJTNEJTIyTnc2RiUyQlNpJTIzeEUlNUJMJTNEJTI0QWZQfk5lJTIyJTNFJTNDbXV0YXRpb24lMjBpdGVtcyUzRCUyMjMlMjIlM0UlM0MlMkZtdXRhdGlvbiUzRSUzQ3ZhbHVlJTIwbmFtZSUzRCUyMkFERDAlMjIlM0UlM0NibG9jayUyMHR5cGUlM0QlMjJ0ZXh0JTIyJTIwaWQlM0QlMjI0JTNGSDRuJTIzQXhmdiUzQTQoZW54JTNEaCU3Q0UlMjIlM0UlM0NmaWVsZCUyMG5hbWUlM0QlMjJURVhUJTIyJTNFRXMlMjBnaWJ0JTIwaW9Ccm9rZXIlMjBBZGFwdGVyJTIwVXBkYXRlcyUzQSUyMCUzQyUyRmZpZWxkJTNFJTNDJTJGYmxvY2slM0UlM0MlMkZ2YWx1ZSUzRSUzQ3ZhbHVlJTIwbmFtZSUzRCUyMkFERDElMjIlM0UlM0NibG9jayUyMHR5cGUlM0QlMjJ0ZXh0X25ld2xpbmUlMjIlMjBpZCUzRCUyMkF6JTI0SC4hdSUzRFM1JTNETCpxUDV4SC0lMjUlMjIlM0UlM0NmaWVsZCUyMG5hbWUlM0QlMjJUeXBlJTIyJTNFJTVDbiUzQyUyRmZpZWxkJTNFJTNDJTJGYmxvY2slM0UlM0MlMkZ2YWx1ZSUzRSUzQ3ZhbHVlJTIwbmFtZSUzRCUyMkFERDIlMjIlM0UlM0NibG9jayUyMHR5cGUlM0QlMjJnZXRfdmFsdWUlMjIlMjBpZCUzRCUyMmZCYTklNDAlNUQxRWtrbG8lMjRGbDEtfkpRJTIyJTNFJTNDZmllbGQlMjBuYW1lJTNEJTIyQVRUUiUyMiUzRXZhbCUzQyUyRmZpZWxkJTNFJTNDZmllbGQlMjBuYW1lJTNEJTIyT0lEJTIyJTNFYWRtaW4uMC5pbmZvLnVwZGF0ZXNMaXN0JTNDJTJGZmllbGQlM0UlM0MlMkZibG9jayUzRSUzQyUyRnZhbHVlJTNFJTNDJTJGYmxvY2slM0UlM0MlMkZ2YWx1ZSUzRSUzQ3ZhbHVlJTIwbmFtZSUzRCUyMlVTRVJOQU1FJTIyJTNFJTNDYmxvY2slMjB0eXBlJTNEJTIydGV4dCUyMiUyMGlkJTNEJTIybXcuJTI0NjVaQzFNOSUyRnklN0RLcGhOJTVFVCUyMiUzRSUzQ2ZpZWxkJTIwbmFtZSUzRCUyMlRFWFQlMjIlM0VDaHJpc3MlM0MlMkZmaWVsZCUzRSUzQyUyRmJsb2NrJTNFJTNDJTJGdmFsdWUlM0UlM0MlMkZibG9jayUzRSUzQyUyRnN0YXRlbWVudCUzRSUzQyUyRmJsb2NrJTNFJTNDJTJGeG1sJTNF
+<xml xmlns="https://developers.google.com/blockly/xml">
+  <block type="on" id="N9hFsB(c0Y]keL{1+0=m" x="63" y="88">
+    <field name="OID">admin.0.info.newUpdates</field>
+    <field name="CONDITION">true</field>
+    <field name="ACK_CONDITION"></field>
+    <statement name="STATEMENT">
+      <block type="telegram" id="YbvO7|4LNS|qDj-g60%J">
+        <field name="INSTANCE">.0</field>
+        <field name="LOG"></field>
+        <field name="SILENT">FALSE</field>
+        <field name="PARSEMODE">default</field>
+        <field name="ESCAPING">FALSE</field>
+        <field name="DISABLE_WEB_PAGE_PREVIEW">FALSE</field>
+        <value name="MESSAGE">
+          <shadow type="text" id="1pXKN7d?y.xaG)y=eqXy">
+            <field name="TEXT">text</field>
+          </shadow>
+          <block type="text_join" id="Nw6F+Si#xE[L=$AfP~Ne">
+            <mutation items="3"></mutation>
+            <value name="ADD0">
+              <block type="text" id="4?H4n#Axfv:4(enx=h|E">
+                <field name="TEXT">Es gibt ioBroker Adapter Updates: </field>
+              </block>
+            </value>
+            <value name="ADD1">
+              <block type="text_newline" id="Az$H.!u=S5=L*qP5xH-%">
+                <field name="Type">\n</field>
+              </block>
+            </value>
+            <value name="ADD2">
+              <block type="get_value" id="fBa9@]1Ekklo$Fl1-~JQ">
+                <field name="ATTR">val</field>
+                <field name="OID">admin.0.info.updatesList</field>
+              </block>
+            </value>
+          </block>
+        </value>
+        <value name="USERNAME">
+          <block type="text" id="mw.$65ZC1M9/y}KphN^T">
+            <field name="TEXT">Chriss</field>
+          </block>
+        </value>
+      </block>
+    </statement>
+  </block>
+</xml>
